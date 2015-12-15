4a4489a9811549c85bd455206fbca988340af52e
vikas <vikas13jun@gmail.com>
2015-12-15 16:22:38 +0530

    Templating system initialized using bootstrap

--- article/views.py
+++ article/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+	#return HttpResponse('hello');
+	return render_to_response('article/index.html');
--- online_content/urls.py
+++ online_content/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'online_content.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('article.urls', namespace='article')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
